96eb6cb8db967c9803dba6de9cb1c40566241aaf
Filip Kozianski <filip.kozianski@oulook.com>
2018-02-16 18:35:12 +0100

    Tabela kanbana pokazuje prasy wraz ze statusem i odpowiednim kolorem dobranym wg kodu postoju. Kolumna COV pokazuje pokrycie wulkanizacji dla zazbrojonych/aktualnie pracujących/planowanych form. Kolumna MOLDS pokazuje ilości form.
    Signed-off-by: Filip Kozianski <filip.kozianski@oulook.com>

--- js/app.js
+++ js/app.js
@@ -3,68 +3,120 @@ var mediumInventory = 12400;
 var minimumInventory = 11700;
 var optimalInventory = 13100;
 
+function test(){
+    alert('AAA');
+    $("#mainPage").load('http://pldpk/bc3/buildingdayreport/dayreport.php');
+};
+
 $(document).ready(function(){
 
-    $.getJSON('php/buildingData.php',function(jasonData){
+    $.getJSON('php/kanbanData.php',function(jasonData){
+       for(i=0;i<Object.keys(jasonData).length;i++)
+       {
+           if(!$('#'+jasonData[i].gt).html())
+           {
+               var actCurCoverage = (jasonData[i].buildingInvOk/(jasonData[i].moldQty*jasonData[i].curePerHour));
+                    actCurCoverage= Math.trunc(actCurCoverage) + ':' + Math.round((actCurCoverage*60)%60);
 
+               var realCurCoverage = (jasonData[i].buildingInvOk/(jasonData[i].realMoldQty*jasonData[i].realCurePerHour));
+               realCurCoverage= Math.trunc(realCurCoverage) + ':' + Math.round((realCurCoverage*60)%60);
 
-            $('#buildingOutputForecast').html(jasonData.bc3output + '(' + jasonData.bc3pred + ')');
-            $('#curingOutputForecast').html(jasonData.bc4output + '(' + jasonData.bc4pred + ')');
-            $('#inventory').html(jasonData.inv_ok + '(' + jasonData.inv_nok + ')');
-            $('#lackTires').html(jasonData.totalNoTires + ' FH');
+               var plannedCurCoverage = (jasonData[i].buildingInvOk/(jasonData[i].plannedMoldQty*jasonData[i].plannedCurePerHour));
+               plannedCurCoverage= Math.trunc(plannedCurCoverage) + ':' + Math.round((plannedCurCoverage*60)%60);
 
-    
-            //TOTAL NO TIRES COLOR
-            if(jasonData.totalNoTires > 50)
-                $('#lackTires').attr('class','danger');
-            else if(jasonData.totalNoTires > 25)
-                $('#lackTires').attr('class','warning');
-            else
-                $('#lackTires').attr('class','success');
+               $('#kanbanTable').append('<tr value="1" class="kanbanRow" id="'+jasonData[i].gt+'"><td>'+jasonData[i].gt+'</td>' +
+                   '<td id="actCoverage">'+actCurCoverage+'</td>' + //pokrycie wulkanizacji wg zazbrojenia w RFBC
+                   '<td id="actCoverage">'+realCurCoverage+'</td>' + //pokrycie wulkanizacji wg aktualnego stanu
+                   '<td id="actCoverage">'+plannedCurCoverage+'</td>' + //pokrycie wulkanizacji wg planu
+                   '<td>'+jasonData[i].moldQty+'/'+jasonData[i].realMoldQty+'/'+jasonData[i].plannedMoldQty+'</td>' +  // ilość form
+                   '<td>'+jasonData[i].buildingMachines+'</td>' +
+                   '<td>'+jasonData[i].buildingInvOk+'('+jasonData[i].buildingInvNok+')</td>' +
+                   '<td></td>' +
+                   '<td style="display:none"></td>' +
+                   '</tr>');
+           }
 
-            //NO TRANSPORT COLOR
-            if(jasonData.bc3Notires > 35)
-                $('#bc3NoTires').attr('class','danger');
-            else if(jasonData.bc3Notires > 20)
-                $('#bc3NoTires').attr('class','warning');
-            else
-                $('#bc3NoTires').attr('class','success');
-             $('#bc3NoTires').html(jasonData.bc3NoTires);
-
-            //NO TRANSPORT COLOR
-            if(jasonData.seitoNoTires > 15)
-                $('#seitoNoTires').attr('class','danger');
-            else if(jasonData.seitoNoTires > 10)
-                $('#seitoNoTires').attr('class','warning');
-            else
-                $('#seitoNoTires').attr('class','success');
-            $('#seitoNoTires').html(jasonData.seitoNoTires);
-
-            if(jasonData.inv_ok <=minimumInventory || jasonData.inv_ok>=maximumInventory)
-                $('#inventoryDiff').attr('class','danger');
-            else if(jasonData.inv_ok<maximumInventory && jasonData.inv_ok >= mediumInventory)
-                $('#inventoryDiff').attr('class','success');
-            else
-                $('#inventoryDiff').attr('class','warning');
-            $('#inventoryDiff').html(jasonData.inv_ok-optimalInventory);
-
-
-            if((jasonData.bc4pred - jasonData.bc4shiftPlan)>0)
-                 $('#bc4Realization').attr('class','success');
-            else
-                $('#bc4Realization').attr('class','danger');
-            $('#bc4Realization').html((jasonData.bc4pred - jasonData.bc4shiftPlan));
-
-            if(jasonData.bc3pred > jasonData.bc4shiftPlan + 100 && jasonData.bc3pred>jasonData.bc4pred)
-                $('#bc3VsBc4').attr('class','success');
-            else if(jasonData.bc3pred > jasonData.bc4shiftPlan)
-                $('#bc3VsBc4').attr('class','warning');
-            else
-                $('#bc3VsBc4').attr('class','danger');
-
-            $('#bc3Realization').html(jasonData.bc3pred - jasonData.bc3plan);
-            $('#bc3VsBc4').html(jasonData.bc3pred - jasonData.bc4pred)
-
+           $('#kanbanTable').append('<tr value="1" id="'+jasonData[i].gt+'presslist" style="">' +
+               '<td style="display:none">A</td>' +
+               '<td style="display:none"></td>' +
+               '<td style="display:none"></td>' +
+               '<td colspan="6" ;">'+jasonData[i].pressess+'</td>' +
+               '<td style="display:none"></td>' +
+               '<td style="display:none"></td>' +
+               '</tr>');
+       }
     });
 
+    //CLICK ON KANBAN ROW - SHOW PRESSESS
+    $('.kanbanRow').on("click", function (){
+        alert('XXX');
+        $(this).next().toggle();
+       // alert($(this).next().html());
+    });
+
+
+
+    //MAIN TABLE FILL
+    $.getJSON('php/buildingData.php',function(jasonData){
+        $('#buildingOutputForecast').html(jasonData.bc3output + '(' + jasonData.bc3pred + ')');
+        $('#curingOutputForecast').html(jasonData.bc4output + '(' + jasonData.bc4pred + ')');
+        $('#inventory').html(jasonData.inv_ok + '(' + jasonData.inv_nok + ')');
+        $('#lackTires').html(jasonData.totalNoTires + ' FH');
+
+
+        //TOTAL NO TIRES COLOR
+        if(jasonData.totalNoTires > 50)
+            $('#lackTires').attr('class','danger');
+        else if(jasonData.totalNoTires > 25)
+            $('#lackTires').attr('class','warning');
+        else
+            $('#lackTires').attr('class','success');
+
+        //NO TRANSPORT COLOR
+        if(jasonData.bc3Notires > 35)
+            $('#bc3NoTires').attr('class','danger');
+        else if(jasonData.bc3Notires > 20)
+            $('#bc3NoTires').attr('class','warning');
+        else
+            $('#bc3NoTires').attr('class','success');
+        $('#bc3NoTires').html(jasonData.bc3NoTires);
+
+        //NO TRANSPORT COLOR
+        if(jasonData.seitoNoTires > 15)
+            $('#seitoNoTires').attr('class','danger');
+        else if(jasonData.seitoNoTires > 10)
+            $('#seitoNoTires').attr('class','warning');
+        else
+            $('#seitoNoTires').attr('class','success');
+        $('#seitoNoTires').html(jasonData.seitoNoTires);
+
+        if(jasonData.inv_ok <=minimumInventory || jasonData.inv_ok>=maximumInventory)
+            $('#inventoryDiff').attr('class','danger');
+        else if(jasonData.inv_ok<maximumInventory && jasonData.inv_ok >= mediumInventory)
+            $('#inventoryDiff').attr('class','success');
+        else
+            $('#inventoryDiff').attr('class','warning');
+        $('#inventoryDiff').html(jasonData.inv_ok-optimalInventory);
+
+
+        if((jasonData.bc4pred - jasonData.bc4shiftPlan)>0)
+            $('#bc4Realization').attr('class','success');
+        else
+            $('#bc4Realization').attr('class','danger');
+        $('#bc4Realization').html((jasonData.bc4pred - jasonData.bc4shiftPlan));
+
+        if(jasonData.bc3pred > jasonData.bc4shiftPlan + 100 && jasonData.bc3pred>jasonData.bc4pred)
+            $('#bc3VsBc4').attr('class','success');
+        else if(jasonData.bc3pred > jasonData.bc4shiftPlan)
+            $('#bc3VsBc4').attr('class','warning');
+        else
+            $('#bc3VsBc4').attr('class','danger');
+
+        $('#bc3Realization').html(jasonData.bc3pred - jasonData.bc3plan);
+        $('#bc3VsBc4').html(jasonData.bc3pred - jasonData.bc4pred)
+    });
+
+
+
 });
+
---
+++ php/kanbanData.php
@@ -0,0 +1,194 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AA62543
+ * Date: 18-01-17
+ * Time: 19:45
+ */
+
+//error_reporting(E_ALL | E_STRICT);
+//ini_set('display_errors', 1);
+$ffmesConnection = oci_connect('minimes_ff_wbr', 'Baza0racl3appl1cs', '172.22.8.47/ORA');
+$h=date('H');
+
+$start=new DateTime();
+$x=$start;
+$start=date_format ( $start, 'Y-m-d');
+$end=new DateTime();
+$end=date_format ( $end, 'Y-m-d');
+$scraps_table='';
+$startHr;
+$endHr;
+$shift=1;
+
+if($h>=6 && $h<14)
+{
+    $date_range="to_date('".$start."  06:05:00','yy-mm-dd hh24:mi:ss') and to_date('".$end. "  13:56:00','yy-mm-dd hh24:mi:ss') ";
+    $startHr=06;
+    $startHr2=6;
+    $endHr=14;
+    $shift=1;
+    $shift_start="to_date('".$start."  06:00:00','yy-mm-dd hh24:mi:ss') ";
+    $shiftEnd="to_date('".$end."  14:00:00','yy-mm-dd hh24:mi:ss') ";
+    $startBuilding=new DateTime();
+    $startBuilding=date_format ( $startBuilding, 'Ymd');
+}
+
+if($h>=14 && $h<22)
+{
+    $date_range="to_date('".$start."  14:05:00','yy-mm-dd hh24:mi:ss') and to_date('".$end. "  21:56:00','yy-mm-dd hh24:mi:ss') ";
+    $shift_start="to_date('".$start."  14:00:00','yy-mm-dd hh24:mi:ss') ";
+    $shiftEnd="to_date('".$end."  22:00:00','yy-mm-dd hh24:mi:ss') ";
+    $startHr=14;
+    $endHr=22;
+    $shift=2;
+
+    $startBuilding=new DateTime();
+    $startBuilding=date_format ( $startBuilding, 'Ymd');
+}
+
+if($h>=22 || $h<6)
+{
+    if($h>=22)
+    {
+        $end=new DateTime();
+        $end->add(new DateInterval('P1D'));
+        $end=date_format ( $end, 'y-m-d');
+        $date_range="to_date('".$start."  22:05:00','yy-mm-dd hh24:mi:ss') and to_date('".$end. "  05:56:00','yy-mm-dd hh24:mi:ss') ";
+        $shift_start="to_date('".$start."  22:00:00','yy-mm-dd hh24:mi:ss') ";
+        $shiftEnd="to_date('".$end."  06:00:00','yy-mm-dd hh24:mi:ss') ";
+        $startBuilding=new DateTime();
+        $startBuilding=date_format ($startBuilding, 'Ymd');
+    }
+    if($h<=6)
+    {
+        $start=new DateTime();
+        $start->sub(new DateInterval('P1D'));
+        $start=date_format ( $start, 'y-m-d');
+        $date_range="to_date('".$start."  22:05:00','yy-mm-dd hh24:mi:ss') and to_date('".$end. "  05:56:00','yy-mm-dd hh24:mi:ss') ";
+        $shift_start="to_date('".$start."  22:00:00','yy-mm-dd hh24:mi:ss') ";
+        $shiftEnd="to_date('".$end."  06:00:00','yy-mm-dd hh24:mi:ss') ";
+        $startBuilding=new DateTime();
+        $startBuilding->sub(new DateInterval('P1D'));
+        $startBuilding=date_format ( $startBuilding, 'Ymd');
+    }
+    $startHr=22;
+    $endHr=6;
+    $shift=3;
+}
+
+//ZMIENNE
+$gtCode='';
+$i=0;
+$l=1;
+$avgCycleTime=0;
+$data=array();
+
+$stidKanban = oci_parse($ffmesConnection,
+    "SELECT distinct gtcode, 
+       press, 
+       curing_schedule, 
+       fh, 
+       cur_plan100, 
+       cur_plan90, 
+       CASE 
+         WHEN cur_cycle_time != 0 THEN cur_cycle_time 
+         WHEN cur_cycle_time = 0 
+              AND cur_plan100 != 0 THEN Round(fh * 60 / cur_plan100, 2) 
+       END                      cur_cycle_time, 
+       active, 
+       cur_last_prod, 
+       cur_last_down, 
+       cur_status, 
+       cur_last_dwn_code, 
+       CASE 
+         WHEN cur_status = 'D' THEN cur_dwn_desc 
+         ELSE 'PRODUKCJA' 
+       END                      PRESS_STATUS, 
+       cur_dwn_col, 
+       buil_schedule, 
+       buil_output, 
+       buil_machines, 
+       inv_ok, 
+       inv_nok, 
+       cur_molds_qty, 
+       buil_groups, 
+       buil_machines_qty, 
+       when_updated, 
+       SUM(CASE 
+             WHEN cur_last_prod < 60 
+                  AND ( ( cur_last_dwn_code != '16100' 
+                           OR cur_status = 'P' ) 
+                        AND active = 1 ) THEN 1 
+             ELSE 0 
+           END) 
+         over ( 
+           PARTITION BY gtcode) REAL_PRESS_QTY, 
+       Count(DISTINCT press) 
+         over ( 
+           PARTITION BY gtcode) PLANNED_PRESS_QTY, 
+       SUM(cur_plan100) 
+         over ( 
+           PARTITION BY gtcode) SUM_PLANNED_PLAN, 
+       SUM(CASE 
+             WHEN cur_last_prod < 60 
+                  AND ( ( cur_last_dwn_code != '16100' 
+                           OR cur_status = 'P' ) 
+                        AND active = 1 ) THEN cur_plan100 
+             ELSE 0 
+           END) 
+         over ( 
+           PARTITION BY gtcode) SUM_REAL_PLAN, 
+       SUM(CASE 
+             WHEN active = 1 THEN cur_plan100 
+             ELSE 0 
+           END) 
+         over ( 
+           PARTITION BY gtcode) SUM_ACT_CT, 
+       SUM(fh) 
+         over ( 
+           PARTITION BY gtcode) SUM_ACT_FH, 
+       SUM(CASE 
+             WHEN cur_last_prod < 60 
+                  AND ( ( cur_last_dwn_code != '16100' 
+                           OR cur_status = 'P' ) 
+                        AND active = 1 ) THEN fh 
+             ELSE 0 
+           END) 
+         over ( 
+           PARTITION BY gtcode) SUM_REAL_FH ,
+        SUM(fh) 
+         over ( 
+        PARTITION BY gtcode) SUM_PLANNED_FH
+FROM   build_kanban 
+WHERE  cur_plan100 > 0 
+ORDER  BY gtcode, 
+          active DESC, 
+          cur_plan100 DESC, 
+          press");
+
+oci_execute($stidKanban);
+
+while ($row = oci_fetch_array($stidKanban, OCI_BOTH))
+{
+    if($gtCode != $row[0] || $i==0)
+    {
+        $data[$row[0]]=array('gt'=> $row[0],'pressess' => '<span class="hintTip" style="background-color:'.$row[13].';">'.$row[1].'<span class="hintText">'.$row[1].' - '.$row[0].'</br>'.$row[2].'</br>'.$row[12].'</br>'.$row[8].'min</span></span>',
+            'FH' => $row[3], 'plan100' => $row[4], 'plan90' => $row[5],
+            'moldQty' => $row[19], 'curePerHour' => round($row[27]/$row[28], 2),
+            'realMoldQty' => $row[23], 'realCurePerHour' => round($row[26]/$row[29], 2),
+            'plannedMoldQty' => $row[24], 'plannedCurePerHour' => round($row[25]/$row[30], 2),
+            'builScheduled' => $row[14], 'buildingOutput' => $row[15], 'buildingInvOk' => $row[17],
+            'buildingInvNok' => $row[18],  'buildingGroup' => $row[20],
+            'activeBuildingMachines' => $row[21] ,'buildingMachines' => $row[16]);
+        $gtCode=$row[0];
+    }
+    else
+    {
+        $data[$gtCode]['pressess'].=($row[7]==0 ? ('<hr>') : ('') ).'<span class="hintTip" style="background-color:'.$row[13].';">'.$row[1].'<span class="hintText">'.$row[1].' - '.$row[0].'</br>'.$row[2].'</br>'.$row[12].'</br>'.$row[8].'min</span></span>';
+    }
+
+    $l++;
+    $i++;
+}
+echo json_encode(array_values($data), JSON_HEX_QUOT | JSON_HEX_TAG);
